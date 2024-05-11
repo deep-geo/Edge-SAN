1f3f911287dc11fea5d8fb9d07982058c361297d
zhaojianqiang <zhaojq7687@joyowo.com>
2024-05-11 15:51:08 +0800

    add DataLoader

---
+++ DataLoader.py
@@ -0,0 +1,230 @@
+import os
+import json
+import random
+import cv2
+import torch
+import numpy as np
+import albumentations as A
+
+from albumentations.pytorch import ToTensorV2
+from tqdm import tqdm
+from utils import get_boxes_from_mask, init_point_sampling
+from torch.utils.data import Dataset
+
+
+class TestingDataset(Dataset):
+    
+    def __init__(self, split_paths, requires_name=True, point_num=1, return_ori_mask=True, prompt_path=None):
+        """
+        Initializes a TestingDataset object.
+        Args:
+            requires_name (bool, optional): Indicates whether the dataset requires image names. Defaults to True.
+            point_num (int, optional): The number of points to retrieve. Defaults to 1.
+            return_ori_mask (bool, optional): Indicates whether to return the original mask. Defaults to True.
+            prompt_path (str, optional): The path to the prompt file. Defaults to None.
+        """
+        self.return_ori_mask = return_ori_mask
+        self.prompt_path = prompt_path
+        self.prompt_list = {} if prompt_path is None else json.load(open(prompt_path, "r"))
+        self.requires_name = requires_name
+        self.point_num = point_num
+
+        self.image_paths = []
+        self.label_paths = []
+
+        for i, split_path in enumerate(split_paths):
+            with open(split_path, "r") as f:
+                split_data = json.load(f)
+
+            data_root = os.path.dirname(split_path)
+            print(f"\nRead test data from: {data_root}")
+            for data_path, label_path in tqdm(split_data["test"]):
+                data_path = os.path.join(data_root, data_path)
+                if isinstance(label_path, str):
+                    label_paths = [os.path.join(data_root, label_path)]
+                else:
+                    label_paths = [os.path.join(data_root, _) for _ in label_path]
+                for path in label_paths:
+                    label_img = cv2.imread(path, cv2.IMREAD_GRAYSCALE)
+                    vals = sorted([val for val in np.unique(label_img) if val != 0])
+                    for val in vals:
+                        self.image_paths.append(data_path)
+                        self.label_paths.append((val, path))
+
+        self.pixel_mean = [123.675, 116.28, 103.53]
+        self.pixel_std = [58.395, 57.12, 57.375]
+    
+    def __getitem__(self, index):
+        """
+        Retrieves and preprocesses an item from the dataset.
+        Args:
+            index (int): The index of the item to retrieve.
+        Returns:
+            dict: A dictionary containing the preprocessed image and associated information.
+        """
+        image_input = {}
+        try:
+            image = cv2.imread(self.image_paths[index])
+            image = (image - self.pixel_mean) / self.pixel_std
+        except:
+            print(self.image_paths[index])
+
+        mask_val, mask_path = self.label_paths[index]
+        ori_np_mask = cv2.imread(mask_path, 0)  # 0 - cv2.IMREAD_GRAYSCALE
+        ori_np_mask[ori_np_mask != mask_val] = 0
+        ori_np_mask[ori_np_mask == mask_val] = 1
+
+        assert np.array_equal(ori_np_mask, ori_np_mask.astype(bool)), f"Mask should only contain binary values 0 and 1. {self.label_paths[index]}"
+
+        h, w = ori_np_mask.shape
+        ori_mask = torch.tensor(ori_np_mask).unsqueeze(0) # ori_mask = torch.tensor(cv2.resize(ori_np_mask, (self.image_size, self.image_size))).unsqueeze(0)
+
+        transforms = A.Compose([ToTensorV2(p=1.0)], p=1.)
+        augments = transforms(image=image, mask=ori_np_mask)
+        image, mask = augments['image'], augments['mask'].to(torch.int64)
+
+        if self.prompt_path is None:
+            boxes = get_boxes_from_mask(mask, max_pixel = 0)
+            point_coords, point_labels = init_point_sampling(mask, self.point_num)
+        else:
+            prompt_key = mask_path.split('/')[-1]
+            boxes = torch.as_tensor(self.prompt_list[prompt_key]["boxes"], dtype=torch.float)
+            point_coords = torch.as_tensor(self.prompt_list[prompt_key]["point_coords"], dtype=torch.float)
+            point_labels = torch.as_tensor(self.prompt_list[prompt_key]["point_labels"], dtype=torch.int)
+
+        image_input["image"] = image
+        image_input["label"] = mask.unsqueeze(0)
+        image_input["point_coords"] = point_coords
+        image_input["point_labels"] = point_labels
+        image_input["boxes"] = boxes
+        image_input["original_size"] = (h, w)
+        image_input["label_path"] = '/'.join(mask_path.split('/')[:-1])
+
+        if self.return_ori_mask:
+            image_input["ori_label"] = ori_mask
+     
+        image_name = self.label_paths[index][1].split('/')[-1]
+        if self.requires_name:
+            image_input["name"] = image_name
+            return image_input
+        else:
+            return image_input
+
+    def __len__(self):
+        return len(self.label_paths)
+
+
+class TrainingDataset(Dataset):
+
+    def __init__(self, split_paths, requires_name=True, point_num=1, mask_num=5):
+        """
+        Initializes a training dataset.
+        Args:
+            requires_name (bool, optional): Indicates whether to include image names in the output. Defaults to True.
+            num_points (int, optional): Number of points to sample. Defaults to 1.
+            num_masks (int, optional): Number of masks to sample. Defaults to 5.
+        """
+        self.requires_name = requires_name
+        self.point_num = point_num
+        self.mask_num = mask_num
+        self.pixel_mean = [123.675, 116.28, 103.53]
+        self.pixel_std = [58.395, 57.12, 57.375]
+
+        self.image_paths = []
+        self.label_paths = []
+
+        for i, split_path in enumerate(split_paths):
+            with open(split_path, "r") as f:
+                split_data = json.load(f)
+
+            data_root = os.path.dirname(split_path)
+            print(f"\nRead train data from: {data_root}")
+            for data_path, label_path in tqdm(split_data["train"]):
+                data_path = os.path.join(data_root, data_path)
+                if isinstance(label_path, str):
+                    label_paths = [os.path.join(data_root, label_path)]
+                else:
+                    label_paths = [os.path.join(data_root, _) for _ in label_path]
+                for path in label_paths:
+                    self.image_paths.append(data_path)
+                    self.label_paths.append(path)
+    
+    def __getitem__(self, index):
+        """
+        Returns a sample from the dataset.
+        Args:
+            index (int): Index of the sample.
+        Returns:
+            dict: A dictionary containing the sample data.
+        """
+
+        image_input = {}
+        try:
+            image = cv2.imread(self.image_paths[index])
+            image = (image - self.pixel_mean) / self.pixel_std
+        except:
+            print(self.image_paths[index])
+
+        h, w, _ = image.shape
+        transforms = A.Compose([ToTensorV2(p=1.0)], p=1.)
+    
+        masks_list = []
+        boxes_list = []
+        point_coords_list, point_labels_list = [], []
+        # mask_path = random.choices(self.label_paths[index], k=self.mask_num)
+
+        mask_path = self.label_paths[index]
+        original_mask = cv2.imread(mask_path, 0)
+        mask_vals = [_ for _ in np.unique(original_mask) if _ != 0]
+        choices_nuclei = random.choices(mask_vals, k=self.mask_num)
+
+        for mask_val in choices_nuclei:
+            pre_mask = original_mask.copy()
+
+            # 只考虑当前细胞核编号，其它编号细胞核都按照背景处理
+            pre_mask[pre_mask != mask_val] = 0.0
+            pre_mask[pre_mask == mask_val] = 1.0
+
+            augments = transforms(image=image, mask=pre_mask)
+            image_tensor, mask_tensor = augments['image'], augments['mask'].to(
+                torch.int64)
+
+            boxes = get_boxes_from_mask(mask_tensor)
+            point_coords, point_label = init_point_sampling(mask_tensor,
+                                                            self.point_num)
+
+            masks_list.append(mask_tensor)
+            boxes_list.append(boxes)
+            point_coords_list.append(point_coords)
+            point_labels_list.append(point_label)
+
+        mask = torch.stack(masks_list, dim=0)
+        boxes = torch.stack(boxes_list, dim=0)
+        point_coords = torch.stack(point_coords_list, dim=0)
+        point_labels = torch.stack(point_labels_list, dim=0)
+
+        image_input["image"] = image_tensor.unsqueeze(0)
+        image_input["label"] = mask.unsqueeze(1)
+        image_input["boxes"] = boxes
+        image_input["point_coords"] = point_coords
+        image_input["point_labels"] = point_labels
+
+        image_name = self.image_paths[index].split('/')[-1]
+        if self.requires_name:
+            image_input["name"] = image_name
+            return image_input
+        else:
+            return image_input
+
+    def __len__(self):
+        return len(self.image_paths)
+
+
+def stack_dict_batched(batched_input):
+    out_dict = {}
+    for k,v in batched_input.items():
+        if isinstance(v, list):
+            out_dict[k] = v
+        else:
+            out_dict[k] = v.reshape(-1, *v.shape[2:])
+    return out_dict
