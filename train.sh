75d1532c470c497b95bb6aa85c156e3d446807ee
zhaojianqiang <zhaojq7687@joyowo.com>
2024-05-17 17:04:57 +0800

    update train.sh

--- train.sh
+++ train.sh
@@ -2,7 +2,7 @@
 
 python train.py \
 --work_dir "workdir" \
---run_name "nuclei-051701" \
+--run_name "nuclei" \
 --seed 42 \
 --epochs 100000 \
 --batch_size 8 \
@@ -10,7 +10,7 @@ python train.py \
 --image_size 256 \
 --mask_num 5 \
 --split_paths "/root/autodl-tmp/datasets/SAM_nuclei_preprocessed/ALL/split.json" \
---metrics 'iou' 'dice' 'precision' 'f1_score' 'recall' 'specificity' 'accuracy' 'hausdorff_distance' \
+--metrics 'iou' 'dice' 'precision' 'f1_score' 'recall' 'specificity' 'accuracy' \
 --device "cuda" \
 --lr 0.0001 \
 --resume "" \
