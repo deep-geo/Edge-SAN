a190d8ba50e9e4d066fc65666f958ccaaf635e08
zhaojianqiang <zhaojq7687@joyowo.com>
2024-05-27 14:14:53 +0800

    update preprocessing script

--- preprocess/preprocess_histology.py
+++ preprocess/preprocess_histology.py
@@ -19,6 +19,7 @@ import glob
 import argparse
 import cv2
 import tqdm
+import numpy as np
 
 from preprocess import Preprocess
 
@@ -39,8 +40,31 @@ class PreprocessHistology(Preprocess):
         label_paths = glob.glob(os.path.join(src_label_dir, "*.png"))
         print("\nProcess label...")
         for path in tqdm.tqdm(label_paths):
-            label = cv2.imread(path, cv2.IMREAD_GRAYSCALE)
-            self.save_label(ori_label=label,
+            src_label = cv2.imread(path, cv2.IMREAD_GRAYSCALE)
+
+            # remove bound
+            src_label[src_label == 76] = 0
+            src_label[src_label == 149] = 0
+
+            contours, _ = cv2.findContours(src_label, cv2.RETR_EXTERNAL,
+                                           cv2.CHAIN_APPROX_SIMPLE)
+
+            label_uint16 = np.zeros(shape=src_label.shape, dtype=np.uint16)
+            for i, contour in enumerate(contours):
+                tmp_label = np.zeros(shape=src_label.shape, dtype=np.uint8)
+                tmp_label = cv2.drawContours(tmp_label, [contour], -1, 255, -1)
+
+                # 膨胀补上边界
+                dilation_kernel = np.ones((3, 3), np.uint8)
+                tmp_label = cv2.dilate(tmp_label, dilation_kernel, iterations=2)
+
+                tmp_contours, _ = cv2.findContours(tmp_label,
+                                                   cv2.RETR_EXTERNAL,
+                                                   cv2.CHAIN_APPROX_SIMPLE)
+                label_uint16 = cv2.drawContours(label_uint16, tmp_contours, -1,
+                                                color=i + 1, thickness=-1)
+
+            self.save_label(ori_label=label_uint16,
                             label_name=os.path.basename(path)[:-4])
 
 
