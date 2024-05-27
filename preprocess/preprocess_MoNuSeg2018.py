9453816c3cfd09d90b681342b7fb875563456f6b
zhaojianqiang <zhaojq7687@joyowo.com>
2024-05-27 18:03:01 +0800

    add preprocessing script for MoNuSeg2018

---
+++ preprocess/preprocess_MoNuSeg2018.py
@@ -0,0 +1,170 @@
+"""
+Script for TNBC dataset preprocessing.
+
+Original dataset structure:
+.
+├── Annotations
+│   ├── TCGA-18-5592-01Z-00-DX1.xml
+│   ├── TCGA-21-5784-01Z-00-DX1.xml
+│   ├── TCGA-21-5786-01Z-00-DX1.xml
+│   ├── TCGA-38-6178-01Z-00-DX1.xml
+│   ├── TCGA-49-4488-01Z-00-DX1.xml
+│   ├── TCGA-50-5931-01Z-00-DX1.xml
+│   ├── TCGA-A7-A13E-01Z-00-DX1.xml
+│   ├── TCGA-A7-A13F-01Z-00-DX1.xml
+│   ├── TCGA-AR-A1AK-01Z-00-DX1.xml
+│   ├── TCGA-AR-A1AS-01Z-00-DX1.xml
+│   ├── TCGA-AY-A8YK-01A-01-TS1.xml
+│   ├── TCGA-B0-5698-01Z-00-DX1.xml
+│   ├── TCGA-B0-5710-01Z-00-DX1.xml
+│   ├── TCGA-B0-5711-01Z-00-DX1.xml
+│   ├── TCGA-BC-A217-01Z-00-DX1.xml
+│   ├── TCGA-CH-5767-01Z-00-DX1.xml
+│   ├── TCGA-DK-A2I6-01A-01-TS1.xml
+│   ├── TCGA-E2-A14V-01Z-00-DX1.xml
+│   ├── TCGA-E2-A1B5-01Z-00-DX1.xml
+│   ├── TCGA-F9-A8NY-01Z-00-DX1.xml
+│   ├── TCGA-FG-A87N-01Z-00-DX1.xml
+│   ├── TCGA-G2-A2EK-01A-02-TSB.xml
+│   ├── TCGA-G9-6336-01Z-00-DX1.xml
+│   ├── TCGA-G9-6348-01Z-00-DX1.xml
+│   ├── TCGA-G9-6356-01Z-00-DX1.xml
+│   ├── TCGA-G9-6362-01Z-00-DX1.xml
+│   ├── TCGA-G9-6363-01Z-00-DX1.xml
+│   ├── TCGA-HE-7128-01Z-00-DX1.xml
+│   ├── TCGA-HE-7129-01Z-00-DX1.xml
+│   ├── TCGA-HE-7130-01Z-00-DX1.xml
+│   ├── TCGA-KB-A93J-01A-01-TS1.xml
+│   ├── TCGA-MH-A561-01Z-00-DX1.xml
+│   ├── TCGA-NH-A8F7-01A-01-TS1.xml
+│   ├── TCGA-RD-A8N9-01A-01-TS1.xml
+│   ├── TCGA-UZ-A9PJ-01Z-00-DX1.xml
+│   ├── TCGA-UZ-A9PN-01Z-00-DX1.xml
+│   └── TCGA-XS-A8TJ-01Z-00-DX1.xml
+└── Tissue Images
+    ├── TCGA-18-5592-01Z-00-DX1.tif
+    ├── TCGA-21-5784-01Z-00-DX1.tif
+    ├── TCGA-21-5786-01Z-00-DX1.tif
+    ├── TCGA-38-6178-01Z-00-DX1.tif
+    ├── TCGA-49-4488-01Z-00-DX1.tif
+    ├── TCGA-50-5931-01Z-00-DX1.tif
+    ├── TCGA-A7-A13E-01Z-00-DX1.tif
+    ├── TCGA-A7-A13F-01Z-00-DX1.tif
+    ├── TCGA-AR-A1AK-01Z-00-DX1.tif
+    ├── TCGA-AR-A1AS-01Z-00-DX1.tif
+    ├── TCGA-AY-A8YK-01A-01-TS1.tif
+    ├── TCGA-B0-5698-01Z-00-DX1.tif
+    ├── TCGA-B0-5710-01Z-00-DX1.tif
+    ├── TCGA-B0-5711-01Z-00-DX1.tif
+    ├── TCGA-BC-A217-01Z-00-DX1.tif
+    ├── TCGA-CH-5767-01Z-00-DX1.tif
+    ├── TCGA-DK-A2I6-01A-01-TS1.tif
+    ├── TCGA-E2-A14V-01Z-00-DX1.tif
+    ├── TCGA-E2-A1B5-01Z-00-DX1.tif
+    ├── TCGA-F9-A8NY-01Z-00-DX1.tif
+    ├── TCGA-FG-A87N-01Z-00-DX1.tif
+    ├── TCGA-G2-A2EK-01A-02-TSB.tif
+    ├── TCGA-G9-6336-01Z-00-DX1.tif
+    ├── TCGA-G9-6348-01Z-00-DX1.tif
+    ├── TCGA-G9-6356-01Z-00-DX1.tif
+    ├── TCGA-G9-6362-01Z-00-DX1.tif
+    ├── TCGA-G9-6363-01Z-00-DX1.tif
+    ├── TCGA-HE-7128-01Z-00-DX1.tif
+    ├── TCGA-HE-7129-01Z-00-DX1.tif
+    ├── TCGA-HE-7130-01Z-00-DX1.tif
+    ├── TCGA-KB-A93J-01A-01-TS1.tif
+    ├── TCGA-MH-A561-01Z-00-DX1.tif
+    ├── TCGA-NH-A8F7-01A-01-TS1.tif
+    ├── TCGA-RD-A8N9-01A-01-TS1.tif
+    ├── TCGA-UZ-A9PJ-01Z-00-DX1.tif
+    ├── TCGA-UZ-A9PN-01Z-00-DX1.tif
+    └── TCGA-XS-A8TJ-01Z-00-DX1.tif
+"""
+import os
+import glob
+import argparse
+import cv2
+import tqdm
+import numpy as np
+import xml.etree.ElementTree as ET
+
+from preprocess import Preprocess
+
+
+def parse_aperio_xml(xml_file):
+    tree = ET.parse(xml_file)
+    root = tree.getroot()
+
+    annotations = []
+
+    # Iterate through all annotations
+    for annotation in root.findall('.//Annotation'):
+        annotation_data = {
+            'id': annotation.get('Id'),
+            'type': annotation.get('Type'),
+            'name': annotation.get('Name'),
+            'regions': []
+        }
+
+        # Iterate through all regions in an annotation
+        for region in annotation.findall('.//Region'):
+            region_data = {
+                'id': region.get('Id'),
+                'type': region.get('Type'),
+                'vertices': []
+            }
+
+            # Iterate through all vertices in a region
+            for vertex in region.findall('.//Vertex'):
+                x = float(vertex.get('X'))
+                y = float(vertex.get('Y'))
+                region_data['vertices'].append((x, y))
+
+            annotation_data['regions'].append(region_data)
+
+        annotations.append(annotation_data)
+
+    return annotations
+
+
+def draw_annotations_on_image(annotations, image_size):
+    image = np.zeros((image_size[0], image_size[1]), dtype=np.uint16)
+    for i, region in enumerate(annotations[0]["regions"]):
+        points = np.array(region['vertices'], np.int32)
+        points = points.reshape((-1, 1, 2))
+        cv2.polylines(image, [points], isClosed=True, color=i + 1, thickness=2)
+        cv2.fillPoly(image, [points], color=i + 1)
+
+    return image
+
+
+class PreprocessMoNuSeg2018(Preprocess):
+
+    def process(self):
+        # data
+        src_data_dir = os.path.join(self.src_root, "Tissue Images")
+        src_data_paths = glob.glob(os.path.join(src_data_dir, "*.tif"))
+        src_label_dir = os.path.join(self.src_root, "Annotations")
+        print("\nProcess data...")
+        for path in tqdm.tqdm(src_data_paths):
+            img = cv2.imread(path)
+            self.save_data(ori_data=img, data_name=os.path.basename(path)[:-4])
+
+            basename = os.path.basename(path)
+            label_path = os.path.join(src_label_dir, basename[:-4] + ".xml")
+            annotations = parse_aperio_xml(label_path)
+            label = draw_annotations_on_image(annotations, img.shape)
+
+            self.save_label(ori_label=label, label_name=basename[:-4])
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--src_root", type=str)
+    parser.add_argument("--dst_root", type=str)
+    parser.add_argument("--dst_size", type=int)
+    parser.add_argument("--dst_prefix", type=str, default="")
+    args = parser.parse_args()
+
+    PreprocessMoNuSeg2018(args.src_root, args.dst_root,
+                          args.dst_size, args.dst_prefix).process()
